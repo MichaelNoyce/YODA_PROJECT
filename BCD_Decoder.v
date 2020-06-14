dafc1245b57a7a7437568eeadee10f5817190ba8
johannejvv <53059600+johannejvv@users.noreply.github.com>
2020-06-14 15:30:43 +0200

    BCD_Decoder to display letters

--- BCD_Decoder.v
+++ BCD_Decoder.v
@@ -1,5 +1,5 @@
 module BCD_Decoder(
-    input [3:0]BCD,
+    input [2:0]BCD,
     output reg [6:0]SevenSegment
 );
 //------------------------------------------------------------------------------
@@ -8,16 +8,19 @@ always @(BCD)
 begin
 case(BCD)
     // gfedcba
-    4'd0 : SevenSegment <= 7'b0111111;  //     a
-    4'd1 : SevenSegment <= 7'b0000110;  //    ----
-    4'd2 : SevenSegment <= 7'b1011011;  //   |   |
-    4'd3 : SevenSegment <= 7'b1001111;  //  f| g |b
-    4'd4 : SevenSegment <= 7'b1100110;  //    ----
-    4'd5 : SevenSegment <= 7'b1101101;  //   |   |
-    4'd6 : SevenSegment <= 7'b1111101;  //  e|   |c
-    4'd7 : SevenSegment <= 7'b0000111;  //    ----
-    4'd8 : SevenSegment <= 7'b1111111;  //      d
-    4'd9 : SevenSegment <= 7'b1101111;
+    
+    4'd0 : SevenSegment <= 7'b1111001; //letter E //     a
+    4'd1 : SevenSegment <= 7'b1110111; //letter R //    ----
+    4'd2 : SevenSegment <= 7'b0111111; //letter O //   |   |
+    4'd3 : SevenSegment <= 7'b1101101; //letter S //  f| g |b
+    4'd4 : SevenSegment <= 7'b0111110; //letter U //    ----
+    4'd5 : SevenSegment <= 7'b0111001; //letter C //   |   |
+    4'd6 : SevenSegment <= 7'b1111111; //all off  //  e|   |c
+                                                  //    ----
+                                                  //      d
+    //These letters can be used in combination to output
+    //SUCCESS or
+    //ERROR
 default: SevenSegment <= 7'b0000000;
 endcase
 end
