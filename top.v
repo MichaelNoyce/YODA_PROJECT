5a76dae68bd5f3725f7439ea539267f9e81acb39
johannejvv <53059600+johannejvv@users.noreply.github.com>
2020-06-14 15:39:38 +0200

    Added success and error messages

--- top.v
+++ top.v
@@ -7,7 +7,7 @@ module top(
     input BTNB, //reset module
 
     output reg [7:0] SEG, //to display 'Error' or 'Done'
-    output reg [3:0] AN, 
+    output reg [6:0] AN, 
     output reg [0:0] s_axis_phase_input,
     output reg [7:0] s_axis_phase_tdata_input,
     output reg [31:0] Y_output
@@ -50,19 +50,21 @@ module top(
         else Start_event <= 1'b0;
     end 
     
-    // registers for storing output message 
-    reg [3:0]letter1=4'd0;
-	reg [3:0]letter2=4'd0;
-	reg [3:0]letter3=4'd0;
-	reg [3:0]letter4=4'd0;
+   // registers for storing output message 
+    reg [2:0]letter1=4'd0; //S //E
+	reg [2:0]letter2=4'd0; //U //R
+	reg [2:0]letter3=4'd0; //C //R
+	reg [2:0]letter4=4'd0; //C //O
+	reg [2:0]letter5=4'd0; //E //R
+	reg [2:0]letter6=4'd0; //S
+	reg [2:0]letter7=4'd0; //S
     
     //Initialize seven segment
-	wire [3:0] SegmentDrivers;
-	wire [7:0] SevenSegment;
+	wire [7:0] SegmentDrivers;
+	wire [6:0] SevenSegment;
 	
-	//TODO get the correct letters displayed in BCD_Decoder
 	SS_Driver SS_Driver1(
-		CLK100MHZ, reset_in, letter1, letter2, letter3, letter4,
+		CLK100MHZ, reset_in, letter1, letter2, letter3, letter4, letter5, letter6,letter7,
 		SegmentDrivers, SevenSegment
 	);
 	
@@ -70,9 +72,9 @@ module top(
 	//Memory A IO
     reg ena = 1; //Port A clock enable (enables read, write and reset)
     reg wea = 0; //Port A write disabled
-    reg [7:0] addra=0; //Port A address //TODO depth of BRAM_A 
+    reg [3:0] addra=0; //Port A address  
     reg [7:0] dina=0; //We're not putting data in, so we can leave this unassigned
-    wire [7:0] douta; //Port A data output (to read) //TODO width of BRAM_A
+    wire [7:0] douta; //Port A data output (to read) 
     reg reset = 1'b1;
     	
 	//instantiate BRAM_A    
@@ -82,7 +84,7 @@ module top(
     wea,      // input wire [0 : 0] wea
     addra,  // input wire [7 : 0] addra
     dina,    // input wire [10 : 0] dina
-    douta  // output wire [10 : 0] douta
+    douta  // output wire [7 : 0] douta
     );
     
     //TODO instantiate BRAM_B	
@@ -99,6 +101,9 @@ cordic_0 sine_instance(
 	//checksum variables
 	reg [31:0] sum=0;
 	reg [7:0] X_input; //8 bit input
+	reg [0:0] success =0;
+	reg [0:0] error=0;
+	integer i=0;
 	//The main logic
 	always @(posedge CLK100MHZ) begin
 	    
@@ -112,8 +117,32 @@ cordic_0 sine_instance(
 	
 	   Y_output <= m_axis_dout_tdata_output;
 	   
-	   if(Start_event)begin
-	   //TODO start logic
+	   if (addra<9)begin
+	       addra=addra+1;
+	   end else begin
+	       addra<=0;
+	   end
+	   //se tthe display values
+	   if(success)begin
+	       letter1<=3;//S
+	       letter2<=4;//U
+	       letter3<=5;//C
+	       letter4<=5;//C
+	       letter5<=0;//E
+	       letter6<=3;//S
+	       letter7<=3;//S
+	   end 
+	   
+	   if(error)begin
+	       letter1<=0;//E
+	       letter2<=1;//R
+	       letter3<=1;//R
+	       letter4<=2;//O
+	       letter5<=1;//R
+	       letter6<=6;//off
+	       letter7<=6;//off
+	       
+	   
 	   end
 	   
        if (reset_in)begin
