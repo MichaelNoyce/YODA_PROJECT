65c29753f2b8462ca318ec81daa80b22201e798d
johannejvv <53059600+johannejvv@users.noreply.github.com>
2020-06-24 20:47:06 +0200

    Updated top with sine core

--- top.v
+++ top.v
@@ -3,18 +3,31 @@
 module top(
 
     input CLK100MHZ,
+    
+    //IO
     input BTNA, //start checksum
     input BTNB, //reset module
-
+    input reset_in, //reset module
+    input success,
+    input error,
+    
+    //CORDIC core
+    input s_axis_phase_tvalid_input,
+    input wire [7:0] X_input, //input to the module
+    output reg [31:0] Y_output,
+    output reg [31:0] sum,
+    
+    //display
     output reg [7:0] SEG, //to display 'Error' or 'Done'
     output reg [6:0] AN, 
-    output reg [0:0] s_axis_phase_input,
-    output reg [7:0] s_axis_phase_tdata_input,
-    output reg [31:0] Y_output
+    
+    //BRAM
+    output reg [7:0] BRAM_result
 );
 
+    //BUTTONS
     //Add the reset  
-    wire reset_in;
+    //wire reset_in;
     Delay_Reset resetTop(CLK100MHZ, BTNA, reset_in);
 
     //Add and debounce the button
@@ -50,7 +63,7 @@ module top(
         else Start_event <= 1'b0;
     end 
     
-   // registers for storing output message 
+    // registers for storing output message 
     reg [2:0]letter1=4'd0; //S //E
 	reg [2:0]letter2=4'd0; //U //R
 	reg [2:0]letter3=4'd0; //C //R
@@ -63,6 +76,7 @@ module top(
 	wire [7:0] SegmentDrivers;
 	wire [6:0] SevenSegment;
 	
+	//TODO get the correct letters displayed in BCD_Decoder
 	SS_Driver SS_Driver1(
 		CLK100MHZ, reset_in, letter1, letter2, letter3, letter4, letter5, letter6,letter7,
 		SegmentDrivers, SevenSegment
@@ -72,57 +86,66 @@ module top(
 	//Memory A IO
     reg ena = 1; //Port A clock enable (enables read, write and reset)
     reg wea = 0; //Port A write disabled
-    reg [3:0] addra=0; //Port A address  
+    reg [3:0] addra=0; //Port A address 
     reg [7:0] dina=0; //We're not putting data in, so we can leave this unassigned
-    wire [7:0] douta; //Port A data output (to read) 
+    wire [7:0] douta; //Port A data output (to read)
     reg reset = 1'b1;
-    	
-	//instantiate BRAM_A    
+       	
+	//instantiate BRAM_A
+	//currently it has 256 samples stored    
     blk_mem_gen_0 new_BRAM (
     CLK100MHZ,    // input wire clka
     ena,      // input wire ena
     wea,      // input wire [0 : 0] wea
     addra,  // input wire [7 : 0] addra
-    dina,    // input wire [10 : 0] dina
+    dina,    // input wire [7 : 0] dina
     douta  // output wire [7 : 0] douta
     );
-    
-    //TODO instantiate BRAM_B	
-	
-//cordic core
-cordic_0 sine_instance(
-   CLK100MHZ,                  // input wire aclk
-   s_axis_phase_input,         // input wire s_axis_phase_tvalid
-   s_axis_phase_tdata_input,   // input wire [7 : 0] s_axis_phase_tdata
-   m_axis_dout_tvalid_output,  // output wire m_axis_dout_tvalid
-   m_axis_dout_tdata_output    // output wire [31 : 0] m_axis_dout_tdata
-);
+    	
+	//cordic core
+    reg [7:0]  s_axis_phase_tdata_input;
+    wire m_axis_dout_tvalid_output;
+    wire [32:0] m_axis_dout_tdata_output;
 
-	//checksum variables
-	reg [31:0] sum=0;
-	reg [7:0] X_input; //8 bit input
-	reg [0:0] success =0;
-	reg [0:0] error=0;
-	integer i=0;
+    cordic_0 sine_instance(
+    CLK100MHZ,                  // input wire aclk
+    s_axis_phase_tvalid_input,         // input wire s_axis_phase_tvalid
+    s_axis_phase_tdata_input,   // input wire [7 : 0] s_axis_phase_tdata
+    m_axis_dout_tvalid_output,  // output wire m_axis_dout_tvalid
+    m_axis_dout_tdata_output    // output wire [31 : 0] m_axis_dout_tdata
+    );
+    
 	//The main logic
 	always @(posedge CLK100MHZ) begin
-	    
 	   AN<= SegmentDrivers;
 	   SEG<=SevenSegment;
+	   BRAM_result <=douta; //read input byte from BRAM_A
 	   
-	   X_input <=douta; //read input byte from BRAM_A
+	   //Sine core 
+	   if(s_axis_phase_tvalid_input) begin
+	       s_axis_phase_tdata_input <= X_input; //input byte to sine module  
+	   end 
+
+	   if (m_axis_dout_tvalid_output) begin
+	       Y_output <= m_axis_dout_tdata_output;
+	   end 
 	   
-	   s_axis_phase_input <= 1'b1; // input wire s_axis_phase_tvalid
-	   s_axis_phase_tdata_input <= X_input; //input byte to sine module  
-	
-	   Y_output <= m_axis_dout_tdata_output;
-	   
+	   //Reset button
+	   if (reset_in)begin
+           sum<=0;
+       end else begin
+           sum <= Y_output + sum;
+       end
+        
+        
+	   //read from BRAM
 	   if (addra<9)begin
 	       addra=addra+1;
 	   end else begin
 	       addra<=0;
 	   end
-	   //se tthe display values
+	   
+	   //set the display values
 	   if(success)begin
 	       letter1<=3;//S
 	       letter2<=4;//U
@@ -144,14 +167,7 @@ cordic_0 sine_instance(
 	       
 	   
 	   end
-	   
-       if (reset_in)begin
-       //TODO reset logic
-       sum<=0;
-       end else begin
-       //TODO implement main logic 
-       end
-
-
+       
+       
     end
 endmodule
