f96c3fec75c953ccbae8c3639668e2c9a8780abf
GianlucaPatrizi <31405944+GianlucaPatrizi@users.noreply.github.com>
2020-06-25 22:12:28 +0200

    Add files via upload
    Golden measure code written in Python

---
+++ Python Comparison 25June/checksum.py
@@ -0,0 +1,27 @@
+# Checksum module
+
+import functions
+from tictocgenerator import tic, toc
+import time
+
+# Declare variables
+x = functions.make_input(100000) # Create input array of n bytes
+checksum = 0
+# wrap_around = 0 # To see if wrap around occurs
+ 
+tic() 
+for i in range(len(x)):
+    x[i] = functions.funct(x[i]) # Array of input bytes in integer form after sine function
+print(x)
+for j in range(len(x)):
+    if (checksum < 2**32): # Overflow condition
+        checksum += x[j]
+    else:
+        # wrap_around += 1
+        checksum = checksum - 2*16 
+        checksum += x[j]
+        
+print(checksum) # Base 10 value of checksum
+print (functions.convert_to_32b(checksum)) # Base 2 value of checksum in 32bit format
+# print(wrap_around) # to see if wrap around had occured
+toc()
